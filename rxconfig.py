01c632794a3d0a628dc4313caf1a0fd648474324
MostafaWahiep <20p2981@eng.asu.edu.eg>
2023-10-29 00:53:56 -0700

    created database schema

--- rxconfig.py
+++ rxconfig.py
@@ -3,5 +3,4 @@ import reflex as rx
 config = rx.Config(
     app_name="twitter",
     db_url="sqlite:///reflex.db",
-    env=rx.Env.DEV,
 )
--- twitter/state/base.py
+++ twitter/state/base.py
@@ -1,10 +1,12 @@
 """Base state for Twitter example. Schema is inspired by https://drawsql.app/templates/twitter."""
 from typing import Optional
 
-from sqlmodel import Field
+from sqlmodel import Field, SQLModel, create_engine
 
 import reflex as rx
-
+from datetime import datetime
+from sqlmodel import Field, Relationship
+from typing import Optional
 
 class Follows(rx.Model, table=True):
     """A table of Follows. This is a many-to-many join table.
@@ -15,17 +17,82 @@ class Follows(rx.Model, table=True):
     followed_username: str = Field(primary_key=True)
     follower_username: str = Field(primary_key=True)
 
+class ValidatorModelLink(rx.Model, table=True):
+    validator_id: int = Field(foreign_key="validator.id", primary_key=True, default=None)
+    model_id: int = Field(foreign_key="model.id", primary_key=True, default=None)
+
+
 class Validator(rx.Model, table=True):
     """A table of Validators."""
-
-    username: str = Field(primary_key=True)
+    id : Optional[int] = Field(default=None, primary_key=True)
+    username: str = Field()
     password: str = Field()
+
+    models: list["Model"] = Relationship(back_populates="validator", link_model=ValidatorModelLink)
 
 class User(rx.Model, table=True):
     """A table of Users."""
-
+    id : Optional[int] = Field(default=None, primary_key=True)
     username: str = Field()
     password: str = Field()
+
+class Label(rx.Model, table=True):
+    """A table of Labels."""
+    id : Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field()
+    model_id: int = Field(foreign_key="model.id")
+    model: Optional["Model"] = Relationship()
+
+class Model(rx.Model, table=True):
+    id : Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field()
+    dataset: str = Field()
+    notebook: str = Field()
+    model_file: str = Field()
+    dataset_size: int = Field()
+    last_update: str = Field(default=None)
+    new_data_counter: int = Field(default=0)
+
+    labels: list[Label] = Relationship(back_populates="model")
+    labeled: list["Labeled"] = Relationship(back_populates="model")
+    validator: Optional[Validator] = Relationship(back_populates="models", link_model=ValidatorModelLink)
+
+
+    def update(self):
+        self.last_update = datetime.now()
+
+    def increment(self):
+        self.new_data_counter += 1
+
+    def reset(self):
+        self.new_data_counter = 0
+
+    def __repr__(self):
+        return f"<Model {self.name}>"
+    
+
+
+
+# Labeled: ID, user_id, model_id, image, expected_label_id, is_validated, validator_id, correct_label_id, is_pushed
+class Labeled(rx.Model, table=True):
+    id : Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id")
+    model_id: int = Field(foreign_key="model.id")
+    image: str = Field()
+    expected_label_id: int = Field(foreign_key="label.id")
+    is_validated: bool = Field(default=False)
+    validator_id: int = Field(default=None, foreign_key="validator.id")
+    correct_label_id: int = Field(default=None, foreign_key="label.id")
+    is_pushed: bool = Field(default=False)
+
+    user: Optional["User"] = Relationship(back_populates="labeled")
+    model: Optional["Model"] = Relationship(back_populates="labeled")
+    expected_label: Optional["Label"] = Relationship(back_populates="labeled")
+    validator: Optional["Validator"] = Relationship(back_populates="labeled")
+    correct_label: Optional["Label"] = Relationship(back_populates="labeled")
+
+    def __repr__(self):
+        return f"<Labeled {self.id}>"
 
 
 class Tweet(rx.Model, table=True):
@@ -33,9 +100,12 @@ class Tweet(rx.Model, table=True):
 
     content: str = Field()
     created_at: str = Field()
-
     author: str = Field()
+    favorite_count: int = Field()
 
+
+engine = create_engine("sqlite:///reflex.db")
+SQLModel.metadata.create_all(engine)
 
 class State(rx.State):
     """The base state for the app."""
